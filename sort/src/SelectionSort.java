89ec9e2830acdc33f30329e109acbde73463b9b0
Koovp <guweiping0210@qq.com>
2022-05-05 15:53:42 +0800

    commit selection sort

--- sort/src/SelectionSort.java
+++ sort/src/SelectionSort.java
@@ -2,26 +2,43 @@
  * 选择排序
  * 基本思想：每一趟从待排序的数据元素中选择最小（或最大）的一个元素作为首元素，直到所有元素排完为止。
  * 给你一个整数数组 nums，请你将该数组升序排列。
+ *
  * @author Koovp
  */
 public class SelectionSort {
 
-    public static int[] nums = {9,6,4,5,8,7,2,3,1,0};
+    public static void sort(Integer[] nums) {
 
-    public static void main(String[] args) {
+        int min;
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1 ; j < nums.length; j++) {
-                if (nums[i] > nums[j]) {
-                    swap(nums, i, j);
+
+            min = i;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[min] > nums[j]) {
+                    min = j;
                 }
             }
-        }
-        for (int i : nums){
-            System.out.print(i + " ");
+
+            if (min != i){
+                swap(nums,i,min);
+            }
         }
     }
 
-    public static void swap(int[] nums, int i, int j) {
+    public static void main(String[] args) {
+
+        int N = 20;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SelectionSort.sort(arr);
+        for (Integer integer : arr) {
+            System.out.print(integer);
+            System.out.print(' ');
+        }
+    }
+
+    public static void swap(Integer[] nums, int i, int j) {
         nums[i] = nums[i] ^ nums[j];
         nums[j] = nums[i] ^ nums[j];
         nums[i] = nums[i] ^ nums[j];
