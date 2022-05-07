68f178f2a2ee62bdec1a3073e63405733d01c03b
Koovp <guweiping0210@qq.com>
2022-05-07 13:54:19 +0800

    commit bubble sort

--- sort/src/BubbleSort.java
+++ sort/src/BubbleSort.java
@@ -2,22 +2,42 @@
  * 冒泡排序
  * 基本思想：比较相邻两元素，将值大的交换到右边（从小到大排序，也可从大到小排序）。
  * 给你一个整数数组 nums，请你将该数组升序排列。
+ *
  * @author Koovp
  */
 public class BubbleSort {
-    public static int[] nums = {9,6,4,5,8,7,2,3,1,0};
 
-    public static void main(String[] args) {
+    public static void sort(int[] nums) {
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0 ; j < i; j++) {
-                if (nums[i] < nums[j]) {
-                    swap(nums, i, j);
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    swap(nums, j, j + 1);
                 }
             }
+            for (Integer integer : nums) {
+                System.out.print(integer);
+                System.out.print(' ');
+            }
+            System.out.println();
         }
-        for (int i : nums){
-            System.out.print(i + " ");
+    }
+
+    public static void main(String[] args) {
+
+        int n = 20;
+        int[] arr = SortTestHelper.generateRandomArray(n, 0, 100000);
+        for (Integer integer : arr) {
+            System.out.print(integer);
+            System.out.print(' ');
         }
+        System.out.println();
+        BubbleSort.sort(arr);
+        for (Integer integer : arr) {
+            System.out.print(integer);
+            System.out.print(' ');
+        }
+
     }
 
     public static void swap(int[] nums, int i, int j) {
